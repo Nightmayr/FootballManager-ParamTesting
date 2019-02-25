2570d6795ae3ba889fa35e10e6f53bdf63da7cda
HamishG96 <Hamish.Goodman@academytrainee.com>
2019-02-25 15:44:30 +0000

    all tests functional

--- src/test/java/test/CreateAccountNoInputParaTest.java
+++ src/test/java/test/CreateAccountNoInputParaTest.java
@@ -30,45 +30,35 @@ import main.CreateAccountEntry;
 import main.CreateAccountResult;
 
 @RunWith(Parameterized.class)
-public class CreateAccountNoNameParaTest {
+public class CreateAccountNoInputParaTest {
 	
 	@Parameters
 	public static Collection<Object[]> inputData() throws IOException {
 		FileInputStream file = new FileInputStream(Constant.FILELOCATION);
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
-		XSSFSheet sheet = workbook.getSheetAt(1);
-		
-		Object[][] obj = new Object[sheet.getPhysicalNumberOfRows()-1][5];
-		
+		XSSFSheet sheet = workbook.getSheetAt(5);
+
+		Object[][] obj = new Object[sheet.getPhysicalNumberOfRows() - 1][2];
+
 		for (int rowNum = 1; rowNum < sheet.getPhysicalNumberOfRows(); rowNum++) {
-			
-				obj[rowNum-1][0] = sheet.getRow(rowNum).getCell(0).getStringCellValue();
-				obj[rowNum-1][1] = sheet.getRow(rowNum).getCell(1).getStringCellValue();
-				obj[rowNum-1][2] = sheet.getRow(rowNum).getCell(2).getStringCellValue();
-				obj[rowNum-1][3] = sheet.getRow(rowNum).getCell(3).getStringCellValue();
-				obj[rowNum-1][4] = rowNum;
-			}
+
+			obj[rowNum - 1][0] = sheet.getRow(rowNum).getCell(0).getStringCellValue();
+			obj[rowNum - 1][1] = rowNum;
+		}
 		workbook.close();
 		return Arrays.asList(obj);
-		
-		}
-	
-	private String email;
-	private String password;
-	private String confirmPassword;
+
+	}
+
 	private String expected;
 	private int rowNum;
 	private WebDriver driver;
-	
-	public CreateAccountNoNameParaTest(String email, String password, String confirmPassword, String expected, int rowNum) {
-		this.email = email;
-		this.password = password;
-		this.confirmPassword = confirmPassword;
+
+	public CreateAccountNoInputParaTest(String expected, int rowNum) {
 		this.expected = expected;
 		this.rowNum = rowNum;
 	}
-	
-	
+
 	@Before
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", Constant.CHROMEDRIVERLOCATION);
@@ -76,59 +66,59 @@ public class CreateAccountNoNameParaTest {
 		chromeoptions.addArguments("--headless");
 		driver = new ChromeDriver(chromeoptions);
 	}
-	
+
 	@Test
 	public void addAccountAttempt() throws IOException, InterruptedException {
 
 		driver.get(Constant.ADDACCOUNTPAGE);
 		CreateAccountEntry addAccountPage = PageFactory.initElements(driver, CreateAccountEntry.class);
-		addAccountPage.addAccountNoName(email, password, confirmPassword);
-		
+		addAccountPage.addAccountNoInput();
+
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-		
+
 		CreateAccountResult addAccountResult = PageFactory.initElements(driver, CreateAccountResult.class);
-		
+
 		FileInputStream file = new FileInputStream(Constant.FILELOCATION);
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
-		XSSFSheet sheet = workbook.getSheetAt(1);
-		
+		XSSFSheet sheet = workbook.getSheetAt(5);
+
 		XSSFRow row = sheet.getRow(rowNum);
 		XSSFCell cell;
-		cell = row.getCell(4);
+		cell = row.getCell(1);
 		if (cell == null) {
-			cell = row.createCell(4);
+			cell = row.createCell(1);
 		}
-		
+
 		cell.setCellValue(addAccountResult.createAccountAttemptText());
-		
+
 		try {
 			assertEquals("Test failure.", expected, addAccountResult.createAccountAttemptText());
-			cell = row.getCell(5);
+			cell = row.getCell(2);
 			if (cell == null) {
-				cell = row.createCell(5);
+				cell = row.createCell(2);
 			}
 			cell.setCellValue("PASS");
 		} catch (AssertionError fail) {
-			cell = row.getCell(5);
+			cell = row.getCell(2);
 			if (cell == null) {
-				cell = row.createCell(5); 
+				cell = row.createCell(2);
 			}
 			cell.setCellValue("FAIL");
 			fail("test failure");
 		} finally {
-		
-		FileOutputStream fileOut = new FileOutputStream(Constant.FILELOCATION);
 
-		workbook.write(fileOut);
-		fileOut.flush();
-		fileOut.close();
-		
-		workbook.close();
-		file.close();	
+			FileOutputStream fileOut = new FileOutputStream(Constant.FILELOCATION);
+
+			workbook.write(fileOut);
+			fileOut.flush();
+			fileOut.close();
+
+			workbook.close();
+			file.close();
 		}
-		
+
 	}
-	
+
 	@After
 	public void teardown() {
 		driver.quit();

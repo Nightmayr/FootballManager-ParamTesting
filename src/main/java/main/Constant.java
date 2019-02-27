791e4a5609706855f3d2818813515cba50f8edaf
HamishG96 <Hamish.Goodman@academytrainee.com>
2019-02-27 17:19:19 +0000

    updated rmeote path

--- src/main/java/main/Constant.java
+++ src/main/java/main/Constant.java
@@ -2,7 +2,7 @@ package main;
 
 public class Constant {
 	
-	public static final String HOSTED_BASE = "http://35.189.117.216:3000/";
+	public static final String HOSTED_BASE = "http://35.189.117.216/";
 	public static final String LOCAL_BASE = "http://localhost:3000/";
 	public static final String ADD_ACCOUNT_PAGE = "register";
 	public static final String LIST_PAGE = "session";
